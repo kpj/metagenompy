9555caf5feb6b8ee5f517cb6be46d26d671d4ac5
kpj <kim.philipp.jablonski@gmail.com>
2021-01-11 14:40:20 +0100

    Format tests

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -5,30 +5,22 @@ import metagenompy
 
 
 def test_rank_frequency_computation():
-    df = pd.DataFrame({
-        'taxid': ['1', '2', '2', '3', '3'],
-        'rank01': ['AA', 'AB', 'AB', 'BA', 'BA'],
-        'rank02': ['A', 'A', 'A', 'B', 'B']
-    })
+    df = pd.DataFrame(
+        {
+            'taxid': ['1', '2', '2', '3', '3'],
+            'rank01': ['AA', 'AB', 'AB', 'BA', 'BA'],
+            'rank02': ['A', 'A', 'A', 'B', 'B'],
+        }
+    )
     freq_dict = metagenompy.compute_rank_frequencies(
-        df,
-        rank_list=['rank01', 'rank02']
+        df, rank_list=['rank01', 'rank02']
     )
 
     assert list(freq_dict.keys()) == ['rank02', 'rank01']
     pdt.assert_series_equal(
         freq_dict['rank01'],
-        pd.Series([
-            2, 1, 2
-        ], index=[
-            'AB', 'AA', 'BA'
-        ], name='rank01')
+        pd.Series([2, 1, 2], index=['AB', 'AA', 'BA'], name='rank01'),
     )
     pdt.assert_series_equal(
-        freq_dict['rank02'],
-        pd.Series([
-            3, 2
-        ], index=[
-            'A', 'B'
-        ], name='rank02')
+        freq_dict['rank02'], pd.Series([3, 2], index=['A', 'B'], name='rank02')
     )
