07b781bdd6f30f453769239e06d5042ffec981eb
kpj <kim.philipp.jablonski@gmail.com>
2021-01-11 15:31:49 +0100

    Update test cases

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,26 +1,35 @@
 import pandas as pd
 import pandas.testing as pdt
 
+import pytest
+
 import metagenompy
 
 
-def test_rank_frequency_computation():
-    df = pd.DataFrame(
-        {
-            'taxid': ['1', '2', '2', '3', '3'],
-            'rank01': ['AA', 'AB', 'AB', 'BA', 'BA'],
-            'rank02': ['A', 'A', 'A', 'B', 'B'],
-        }
+@pytest.mark.parametrize('df_input,expected_output', [
+    (
+        pd.DataFrame(
+            {
+                'taxid': ['1', '2', '2', '3', '3'],
+                'rank01': ['AA', 'AB', 'AB', 'BA', 'BA'],
+                'rank02': ['A', 'A', 'A', 'B', 'B'],
+            }
+        ),
+        [
+            pd.Series([3, 2], index=['A', 'B'], name='rank02'),
+            pd.Series(
+                [2, 1, 2],
+                index=['AB', 'AA', 'BA'],
+                name='rank01'
+            ),
+        ]
     )
-    freq_dict = metagenompy.compute_rank_frequencies(
-        df, rank_list=['rank01', 'rank02']
+])
+def test_rank_frequency_computation(df_input, expected_output):
+    freq_list = metagenompy.compute_rank_frequencies(
+        df_input, rank_list=df_input.drop('taxid', axis=1).columns
     )
 
-    assert list(freq_dict.keys()) == ['rank02', 'rank01']
-    pdt.assert_series_equal(
-        freq_dict['rank01'],
-        pd.Series([2, 1, 2], index=['AB', 'AA', 'BA'], name='rank01'),
-    )
-    pdt.assert_series_equal(
-        freq_dict['rank02'], pd.Series([3, 2], index=['A', 'B'], name='rank02')
-    )
+    assert len(freq_list) == len(expected_output)
+    for freq, expected_freq in zip(freq_list, expected_output):
+        pdt.assert_series_equal(freq, expected_freq)
