ee9e4d7589e9515a66a44b975470127df3d444f2
kpj <kim.philipp.jablonski@gmail.com>
2021-01-11 13:11:48 +0100

    Set __version__

--- metagenompy/__init__.py
+++ metagenompy/__init__.py
@@ -1,2 +1,12 @@
+# set version
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata
+
+__version__ = metadata.version('metagenompy')
+
+# package imports
 from .taxonomy import *
 from .visualization import *
